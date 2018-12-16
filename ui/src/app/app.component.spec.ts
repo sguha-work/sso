16ae0dd0a6e1c716a1a7bef1f532b6c280052853
sguha-work <sguha1988.life@gmail.com>
2018-12-16 16:12:21 +0530

    ui work continues

--- ui/src/app/app.component.spec.ts
+++ ui/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ui'`, () => {
+  it(`should have as title 'sso'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ui');
+    expect(app.title).toEqual('sso');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ui!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to sso!');
   });
 });
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BeforeLoginHeaderComponent } from './components/before-login-header/before-login-header.component';
+import { AfterLoginHeaderComponent } from './components/after-login-header/after-login-header.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BeforeLoginHeaderComponent,
+    AfterLoginHeaderComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
